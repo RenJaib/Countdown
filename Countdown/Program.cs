1e5cabfb715e1d677724c4400a1b929e9cfe693d
Renju DEvassy <renjudevasy@gmail.com>
2018-10-31 22:53:42 +1300

    Add project files.

---
+++ Countdown/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Countdown
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var searchTerm = "milk";
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("disable-infobars");
+            var driver = new ChromeDriver(options); // new instance of browser chrome
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); //explicit wait time
+            driver.Navigate().GoToUrl("https://shop.countdown.co.nz/"); //navigate to website
+            driver.Manage().Window.Maximize(); //maximise chrome browser window
+            wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElementById("search"))).SendKeys(searchTerm);
+            driver.FindElementById("searchIcon").Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("search-content")));
+            Screenshot searchResultScreenShot = ((ITakesScreenshot)driver).GetScreenshot(); // take screenshot
+            WriteToWord.CreateDocument(@"c:\Users\Renju\Documents\Screenshot" + DateTime.Now.Ticks.ToString() + ".docx", searchTerm, searchResultScreenShot.AsByteArray);
+            driver.Quit();
+        }
+    }
+}
---
+++ Countdown/WriteToWord.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Countdown
+{
+    public class WriteToWord
+    {
+        public static void CreateDocument(string docFilePath, string term, byte[] image)
+        {
+            Microsoft.Office.Interop.Word._Application oWord = new Microsoft.Office.Interop.Word.Application(); //requires to add Microsoft Office reference
+            Microsoft.Office.Interop.Word._Document oDoc;
+            oDoc = oWord.Documents.Add();
+            
+            //To see whats going on while populating the word document set Visible = true
+            oWord.Visible = true;
+
+            //Insert text
+            Object oMissing = System.Reflection.Missing.Value;
+            var oPara1 = oDoc.Content.Paragraphs.Add(ref oMissing);
+            oPara1.Range.Text = term;
+            oPara1.Range.InsertParagraphAfter();
+
+            //Here is the trick to insert a picture from a byte array into MS Word you need to 
+            //convert the ByteArray into an Image and using the Clipboard paste it into the document
+            Image sparePicture = ByteArrayToImage(image);
+            Clipboard.SetDataObject(sparePicture); //requires to add reference system.windows.forms //copies the image similar to cntrl+c
+            var oPara2 = oDoc.Content.Paragraphs.Add(ref oMissing);
+            oPara2.Range.Paste(); //paste the image
+            oPara2.Range.InsertParagraphAfter();
+            oDoc.SaveAs(docFilePath); //save to new document
+            oDoc.Close();
+            oWord.Quit();
+        }
+
+        public static Image ByteArrayToImage(byte[] byteArrayIn)
+        {
+            using (MemoryStream ms = new MemoryStream(byteArrayIn))
+            {
+                Image returnImage = Image.FromStream(ms);
+                return returnImage;
+            }
+        }
+
+        //Method I use to test loading images from disk into byte[]'s and inserting them into word
+        public byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            byte[] result = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                result = ms.ToArray();
+            }
+            return result;
+        }
+    }
+}
